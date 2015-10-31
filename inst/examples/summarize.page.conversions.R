f947741326c0e6cc087c755d522bd9326eaf6241
antagomir <leo.lahti@iki.fi>
2015-10-31 20:59:36 -0200

    ok

--- inst/examples/summarize.page.conversions.R
+++ inst/examples/summarize.page.conversions.R
@@ -1,7 +1,9 @@
 print("Summarize page conversions")
 tab <- cbind(
 	     #pages.parts = df$document.pages.parts, 
-	     pages.total = df$pagecount, 
+	     pages.total = df$pagecount,
+	     volumes = as.character(df$volcount), 
+             parts = as.character(df$parts), 	     
 	     pages.original = as.character(df.orig$physical_extent)
 	     )
 tmp <- write_xtable(tab, paste(output.folder, "documentpages-estimated.csv", sep = ""))
@@ -21,8 +23,6 @@ print("Summarize page and size conversions in a file")
 tab <- cbind(gatherings = as.character(df$gatherings),
 	     width = as.character(df$width),
 	     height = as.character(df$height),
-	     volumes = as.character(df$volcount), 
-             parts = as.character(df$parts), 
 	     dimensions.original = as.character(df.orig$physical_dimension)
 	     )
 tmp <- write_xtable(tab, paste(output.folder, "dimension_conversions.csv", sep = ""))
