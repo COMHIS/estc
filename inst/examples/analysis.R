dbd4fcca111e73e7a6e4caa5148a5d10d33c181a
antagomir <leo.lahti@iki.fi>
2016-03-04 19:55:15 +0100

    ok

--- inst/examples/analysis.R
+++ inst/examples/analysis.R
@@ -21,6 +21,27 @@ df <- readRDS("estc.Rds")
 print("Summary")
 knit("summary.Rmd")
 
+# ------------------------------------------
+
+# ESTC: TODO / Old material
+
+# Custom definitions
+source("general.R") # See estc todo file
+#knit("Edinburgh.Rmd")
+#knit("NorthAmerica.Rmd")
+#knit("Scotland.Rmd")
+#knit("Princeton.Rmd")
+#knit("pagecounts.Rmd")
+#knit("20150624-LIBER.Rmd") # For conference
+#knit("20151023-LIBER.Rmd") # For journal
+#knit("20150611-Paris.Rmd") # For workshop
+
+# Archived:
+# source("timeplot.R") # History vs. all ESTC docs
+# source("duplicates.R")
+
+# -------------------------------------------------
+
 system("git add -f figure/*.png")
 # system("git add -f output.tables/*.csv")
 system("git add output.tables/*.csv")
