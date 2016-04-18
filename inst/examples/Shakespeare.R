d89c16036188d287f42aa2c625c14c5586605fa8
antagomir <leo.lahti@iki.fi>
2016-04-18 23:27:01 +0200

    ok

--- inst/examples/Shakespeare.R
+++ inst/examples/Shakespeare.R
@@ -24,6 +24,32 @@ timespan <- c(1460, 1830)
 df.preprocessed <- filter(df, publication_year >=  min(timespan) & publication_year <= max(timespan))
 rm(df)
 
+# ------------------------------------------
+
+# Read the manually polished data
+
+library(gdata)
+my.authors <- c(Shakespeare = "Shakespeare, William (1564-1616)",
+	        Cervantes = "Cervantes Saavedra, Miguel De (1547-1616)")
+
+# Polished Shakespeare and Cervantes
+tabs <- list()
+for (auth in names(my.authors)) {
+  tab <- read.xls(paste(auth, "_estc.xls", sep = ""))
+  inds <- grep("NOT TO BE", tab$title)
+  if (length(inds) > 0) {
+    tab <- tab[-inds,]
+  }
+  tab$publication_decade <- floor(tab$publication_year/10) * 10   
+  tabs[[auth]] <- tab
+}
+
+# Full data, excluding the selected authors
+dff <- df.preprocessed %>% filter(!author %in% my.authors)
+
+# ------------------------------------------
+
+# Summary document
 
 knit("Shakespeare.Rmd")
 
