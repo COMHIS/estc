d7408785de7b3c535b33fec007438b2d929aeb7e
antagomir <leo.lahti@iki.fi>
2016-04-23 15:28:19 +0200

    ok

---
+++ inst/examples/Shakespeare400.R
@@ -0,0 +1,16 @@
+# Creating initial Shakespeare/Cervantes data for manual checking
+# source("Shakespeare_data.R")
+
+# ------------------------------------------
+
+# Read the manually polished data
+source("Shakespeare.init.R")
+
+# ------------------------------------------
+
+# Summary document
+
+knit("Shakespeare.Rmd")
+
+# Shakespearen kontinenttianalyysi
+# Paperinkulutus
