9ec7eebf89fdad39faf66fcf57bc79c1dba11e14
antagomir <leo.lahti@iki.fi>
2016-06-14 00:14:26 +0200

    ok

--- R/firstlib.R
+++ R/firstlib.R
@@ -3,10 +3,14 @@
 #' @importFrom bibliographica harmonize_print_statements
 #' @importFrom bibliographica read_synonymes
 #' @importFrom bibliographica check_synonymes
-#' @importFrom stringr str_trim
 #' @import dplyr
 #' @import ggplot2
-
+#' @importFrom stats median
+#' @importFrom stats na.omit
+#' @importFrom stringr str_trim
+#' @importFrom utils download.file
+#' @importFrom utils read.csv
+#' @importFrom utils unzip
 
 .onAttach <- function(lib, pkg)
 {
@@ -14,7 +18,6 @@
   # This may help with encodings in Mac/Linux
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
-
   packageStartupMessage("estc - British Library ESTC Data Collection Tools.\nCopyright (C) 2014-2016 Leo Lahti, Niko Ilomaki, Mika Koistinen and Mikko Tolonen\n\nhttps://github.com/ropengov/estc \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
 
 }
