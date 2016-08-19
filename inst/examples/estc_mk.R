c234a62968b5d565e939b9a6894f711bde46e396
jmokoistinen <jmokoistinen@gmail.com>
2016-08-19 16:00:39 +0300

    estc_mk.R

--- inst/examples/estc_mk.R
+++ inst/examples/estc_mk.R
@@ -3,28 +3,34 @@ source.data.file <- "data/estc.csv.gz"
 # Install and load the required custom libraries
 library(devtools)
 install_github("ropengov/sorvi")
-#install.packages(c("gender", "genderdata"),
-#                 repos = "http://packages.ropensci.org",
-#                 type = "source")
-
+install.packages(c("gender", "genderdata"),
+                 repos = "http://packages.ropensci.org",
+                 type = "source")
 install_github("ropengov/bibliographica")
 install_github("ropengov/estc")
 
-library(estc)
+#library(estc)
 library(bibliographica)
 
 # Read the raw data
-source.data.file= 'estc.csv'
+source.data.file= '/home/mckoistt/Documents/ESTC/estc-master/estc.csv'
 df.orig <- read_bibliographic_metadata(source.data.file)
+df100 = df.orig[0:100,]
+df1000 = df.orig[0:1000,]
 
 # Load the polishing function
-source("/home/mckoistt/Documents/ESTC/polish_publisher_forby.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/polish_publisher_forby.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/pick_print_fields.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/pick_shortest_text.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/remove_ending_chars.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/split_doer_and_place.R") # Modify freely polish_forby.R
+source("/home/mckoistt/Documents/r_scripts_mk/ESTC__/estc/R/choose_first_index.R") # Modify freely polish_forby.R
 
 # Polish the publisher field
 # TODO Printed by to be added
-
-pub <- polish_publisher_forby(df.orig$publisher)
-
+pub100 = polish_publisher_forby(df100$publisher)
+pub1000 = polish_publisher_forby(df1000$publisher)
+pub = polish_publisher_forby(df.orig$publisher)
 # Write summaries:
 ## Publishers ordered from most to least common
 
