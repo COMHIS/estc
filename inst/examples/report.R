68945eb7280e44b2d738c8db12c525a64019495b
antagomir <leo.lahti@iki.fi>
2016-10-02 22:53:42 +0200

    ok

--- inst/examples/report.R
+++ inst/examples/report.R
@@ -1,4 +1,4 @@
 
 library(rmarkdown)
-rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "Consciousness entries", data = "df.Rds", id.file = "estc_id_list.txt"))
+rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "Consciousness entries", data = "df.Rds", idsource = "estc_id_list.txt"))
 
