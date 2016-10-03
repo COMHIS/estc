82b722e7b9c1273bbb02ef6729ef15d5c76d5fac
antagomir <leo.lahti@iki.fi>
2016-10-03 22:14:46 +0200

    updates to report.Rmd

--- inst/examples/report.R
+++ inst/examples/report.R
@@ -1,4 +1,4 @@
 
 library(rmarkdown)
-rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "Consciousness entries", idsource = "estc_id_list.txt"))
+rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "My subtitle", idsource = "history_of_england_headings.txt"))
 
