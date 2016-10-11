44c20e9acab456ea5ffaaefec985883911f35f0e
antagomir <leo.lahti@iki.fi>
2016-10-11 17:57:38 +0200

    report modified - forgot to do this in side branch

--- inst/examples/report.R
+++ inst/examples/report.R
@@ -1,5 +1,5 @@
 
 library(rmarkdown)
-rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "My subtitle", idsource = "freewill.txt", update.bibliographica = FALSE))
+rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "My subtitle", idsource = "freewill.txt", update.bibliographica = FALSE, time.window = 10))
 #rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "My subtitle", idsource = "history_of_england_headings.txt", update.bibliographica = TRUE))
 # system("evince report.pdf")
