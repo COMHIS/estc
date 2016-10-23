7c3949e6396d7b13357384561ec5c561e85c5c71
antagomir <leo.lahti@iki.fi>
2016-10-23 14:41:03 +0200

    report extended

--- inst/examples/report.R
+++ inst/examples/report.R
@@ -1,6 +1,6 @@
 
 library(rmarkdown)
-rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, place = "All", language = "any", subtitle = "My subtitle", idsource = "freewill.txt", update.bibliographica = FALSE, time.window = 10))
+rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, place = "All", language = "any", subtitle = "My subtitle", idsource = "freewill.txt", update.bibliographica = FALSE, time.window = 10, document.type = "All"))
 
 #rmarkdown::render("report.Rmd", params = list(min.year = 1470, max.year = 1880, subtitle = "My subtitle", idsource = "history_of_england_headings.txt", update.bibliographica = TRUE))
 # system("evince report.pdf")
