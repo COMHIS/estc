2a3312a5285374984835db3a907e2e005fcc0032
antagomir <leo.lahti@iki.fi>
2018-08-27 17:40:26 +0200

    ok

--- inst/examples/new/master.R
+++ inst/examples/new/master.R
@@ -2,8 +2,8 @@
 
 # note: order matters
 # process_dimension.R # Indepdedent / Runs through with test set -> now migrating to COMHIS/estc-physicaldimension
-
 process_pagecount.R # Depends: physical_dimension / Runs through with test set
+
 process_language.R  # Indepdedent / Runs through with test set
 process_publication_time.R # Independent / finalize
 process_publication_frequency.R # Independent / finalize
@@ -14,7 +14,7 @@ process_note_dissertation.R # Independent / finalize
 process_corporate.R # Independent / finalize
 process_note_granter.R # Independent / finalize
 process_generic.R # Trivial: control_number, system_control_number; TODO: add other simple ones
-author_name, author_date? -> Mark Hill; ask if Mark also included author_date data from the catalogs and how it was polished; if it was not, we can use the earlier polishing codes that were done for this field; how about pseudonymes, we also had code for that with Mikko!
+author_name, author_date? -> Mark Hill; will add author_date; how about pseudonyms -> someone could add based on our earlier code
 publisher   -> Ville
 editions, title, title_uniform, title_uniform2     -> Ali; does this include titles as well, and which fields exactly?
 publication_place, subject_geography, publication_geography -> Iiro Tiihonen
