321d54bf0b82213f4d6f1faa3fb5c1feb5170be2
antagomir <leo.lahti@iki.fi>
2020-03-17 15:17:26 +0200

    ok

--- R/firstlib.R
+++ R/firstlib.R
@@ -18,7 +18,7 @@
   # This may help with encodings in Mac/Linux
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
-  packageStartupMessage("Tools for the British Library ESTC Data Collection.\nCopyright (C) 2014-2017 Leo Lahti, Ville Vaara, Mika Koistinen and Mikko Tolonen\n\nhttps://github.com/ropengov/estc \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
+  packageStartupMessage("Tools for the British Library ESTC Data Collection.\nCopyright (C) 2014-2020 Leo Lahti, Ville Vaara, and Mikko Tolonen\n\nhttps://github.com/ropengov/estc \n\n Some scientists will seek and develop new collaborative arrangements; have members from all fields of science; and these new ways of working, effectively instrumented by huge computers, will contribute greatly to the advance in handling the complex, but essentially organic, problems of the biological and social sciences. - Weaver 1948 \n")
 
 }
 
